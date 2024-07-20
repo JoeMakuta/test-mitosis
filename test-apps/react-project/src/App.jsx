9b2e64d8f24075da3027190450e74cceb93bfc3b
JoeMakuta <makutajosue@gmail.com>
2024-07-20 16:00:20 +0200

    feat : test mitosis

---
+++ test-apps/react-project/src/App.jsx
@@ -0,0 +1,13 @@
+import MyComponent from "../../../output/react-component.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      This app is for testing Mitosis
+      <MyComponent />
+    </>
+  );
+}
+
+export default App;
